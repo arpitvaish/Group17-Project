e15bf8d39784bd43549ca406e02cf78c65b53cb3
omkarkhambekar <khambekaromkar123@gmail.com>
2023-11-11 14:26:03 +0530

    Integration Test with H2 and Eureka server details added

---
+++ UserManagementService/src/src/main/java/dev/omkar/usermanagement/UserManagementApplication.java
@@ -0,0 +1,15 @@
+package dev.omkar.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+}
